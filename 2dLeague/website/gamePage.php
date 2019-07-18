bd2366105ca29188b0dd169e2f4629e13a268b4f
Harmanjyot <37540756+Harmanjyot@users.noreply.github.com>
2019-07-18 16:10:51 +0530

    gamePage

--- 2dLeague/website/gamePage.php
+++ 2dLeague/website/gamePage.php
@@ -19,10 +19,10 @@
 	var ctx = document.getContext("2d");
 
 	ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+	ctx.rect(20, 40, 50, 50);
+	ctx.fillStyle = "#FF0000";
+	ctx.fill();
+	ctx.closePath();
 </script>
 
 </body>
