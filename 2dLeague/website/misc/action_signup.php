25a42942e4af3630070f0dbc05d8d19c481d013b
Harmanjyot <37540756+Harmanjyot@users.noreply.github.com>
2019-09-19 16:23:59 +0530

    i hate my life

--- 2dLeague/website/misc/action_signup.php
+++ 2dLeague/website/misc/action_signup.php
@@ -6,16 +6,23 @@ require "../php/conn.php";
 $name = mysqli_escape_string($conn, $_POST["username"]);
 $email = mysqli_escape_string($conn, $_POST["email"]);
 $pass = mysqli_escape_string($conn, $_POST["password1"]);
-// $email = mysqli_escape_string($_POST["email"]);
+
+$query = "SELECT * FROM playerDetails WHERE userName = '$name' OR emailID = '$email'";
+$result = mysqli_query($conn, $query);
+
+if (mysqli_num_rows($result)>0) {
+	echo "Email or Username already in use.";
+}
+else
+{
+	$hashPass = password_hash($pass, PASSWORD_DEFAULT);
+	$sql = "INSERT INTO playerDetails(userName, emailID, Password) VALUES(?,?,?)";
+	$stmt = mysqli_stmt_init($conn);
+	mysqli_stmt_prepare($stmt, $sql);
+	mysqli_stmt_bind_param($stmt,"sss", $name, $email, $hashPass);
+	mysqli_stmt_execute($stmt);
+	echo "Account created";
+}
+
 
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-
-</body>
-</html>
