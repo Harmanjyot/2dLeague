8384df2590b61d1845ddef454728c23f47afc960
ritsonmathews <47053773+ritsonmathews@users.noreply.github.com>
2019-10-18 14:43:47 +0530

    Dash Latest Update LEGENDARY!!!

--- website/dash.php
+++ website/dash.php
@@ -4,53 +4,52 @@
  if (isset($_SESSION["playerID"])) {
 
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Begin!</title>
+    <title>BEGIN!</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-
-    <link rel='stylesheet' type='text/css' media='screen' href='css/login.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/dash.css'>
     <script src='main.js'></script>
 </head>
 <body>
     <div id="title">
-        <p id="bar"></p>
+        
         <h1>2D League</h1>
-        <div style="float: right;">
-            <form method="POST" action="misc/action_logout.php">
-                <button id="logout" type="submit" name="logout">LOGOUT</button>
-            </form>
-            <form method="POST" action="stats.php">
-                <button id="logout" type="submit" name="logout">STATS</button>
-            </form>
-        </div>
-        
-        
-    </div>
-    <div class="container">
-        <form action="2dpong/public/gamepage.php" method="POST">
-                  <div  id="grad">
-                     
-                    <div class="row">
-                    <h2 style="text-align:center">Enter username of player 2 to begin!</h2>
-                      <input type="text" name="username" placeholder="Username" required>
-                      <input type="password" name="password" placeholder="Password" required>
-                      <br>
-                      <centre><button type="submit" name="Login">Play</button></centre>
-                      <br>
-                    </div>
-                  </div>
-
+        <form method="POST" action="misc/action_logout.php">
+        <button id="logout" type="submit" name="logout">LOGOUT</button>
+        </form>
+        <form method="POST" action="stats.php">
+            <button id="logout" type="submit" name="logout">STATS</button>
         </form>
     </div>
+    <br><br><br><br>
+    <div id="grad">
+        <div id="buton">
+            <h2>Hi! Infant Drake</h2>
+            <div class="container">
+                <form action="2dpong/public/gamepage.php" method="POST">
+                          <div  id="grad1">
+                             
+                            <div class="row">
+                            <h3 style="text-align:center">Enter username of player 2 to begin!</h3>
+                              <input type="text" name="username" placeholder="Username" required>
+                              <input type="password" name="password" placeholder="Password" required>
+                              <br>
+                              <centre><button id="play" type="submit" name="Login">Play</button></centre>
+                              <br>
+                            </div>
+                          </div>
+        
+                </form>
+            </div>
+        </div>
     
+    </div>
     </body>
 </html>
-
 <?php
 }
 ?>
