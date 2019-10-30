e584c7c57131194970f0ecd8fa05acad7c05bd9e
finchflinch <40954549+finchflinch@users.noreply.github.com>
2019-10-30 13:51:00 +0530

    deleted mainfolder gamepage

--- website/2dpong/public/gamepage.php
+++ website/2dpong/public/gamepage.php
@@ -25,10 +25,9 @@ if(isset($_SESSION["playerID"]))
 <head>
   <meta charset="UTF-8">
   
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.dev.js"></script>
+  <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.dev.js"></script> -->
   <script language="javascript" type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/p5.min.js"></script>
   <script language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/addons/p5.dom.min.js"></script>
-  <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/addons/p5.sound.min.js"></script> -->
   <script type="text/javascript">
   var p2name = "<?php echo $player2Name ?>";
   </script>
