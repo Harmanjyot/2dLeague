5dbc0d71abdb15d8db44eeb0ce5c39630e695ed7
ritsonmathews <47053773+ritsonmathews@users.noreply.github.com>
2019-10-30 11:46:54 +0530

    check

--- website/dash.php
+++ website/dash.php
@@ -1,14 +1,14 @@
 <?php
-//  require "php/conn.php";
-//  session_start(); 
-//  if (isset($_SESSION["playerID"])) {
-//      $sql = "SELECT * FROM playerDetails where userID = ?";
-//      $stmt = mysqli_stmt_init($conn);
-//         mysqli_stmt_prepare($stmt, $sql);
-//         mysqli_stmt_bind_param($stmt,"i", $_SESSION["playerID"]);
-//         mysqli_stmt_execute($stmt);
-//         $result = $stmt->get_result();
-//         $row = mysqli_fetch_array($result);
+ require "php/conn.php";
+ session_start(); 
+ if (isset($_SESSION["playerID"])) {
+     $sql = "SELECT * FROM playerDetails where userID = ?";
+     $stmt = mysqli_stmt_init($conn);
+        mysqli_stmt_prepare($stmt, $sql);
+        mysqli_stmt_bind_param($stmt,"i", $_SESSION["playerID"]);
+        mysqli_stmt_execute($stmt);
+        $result = $stmt->get_result();
+        $row = mysqli_fetch_array($result);
 
 ?>
 <!DOCTYPE html>
@@ -23,8 +23,9 @@
 <body>
 
     <div id="title">
-     <div id="overlay" onclick="off()"></div>
-        <h1>2D League</h1>
+     <div id="overlay" onclick="off()">
+     </div>
+        <h1>2D League</h1> 
         <form method="POST" action="misc/action_logout.php">
         <button id="logout" type="submit" name="logout">LOGOUT</button>
         </form>
@@ -56,18 +57,18 @@
             </div>
         </div>
     
-    </div>
-    <script>
-function on() {
-  document.getElementById("overlay").style.display = "block";
-}
+      </div>
+        <script>
+       function on() {
+        document.getElementById("overlay").style.display = "block";
+        }
 
-function off() {
-  document.getElementById("overlay").style.display = "none";
-}
-</script>
+        function off() {
+        document.getElementById("overlay").style.display = "none";
+         }
+        </script>
     </body>
 </html>
 <?php
-//  }
+ }
 ?>
