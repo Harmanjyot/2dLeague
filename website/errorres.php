af84f5ad05145f2c55c198938bf946557bea1f1c
ritsonmathews <47053773+ritsonmathews@users.noreply.github.com>
2019-11-03 14:34:25 +0530

    errorres.php

---
+++ website/errorres.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Error</title>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/error.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+  <div id="outer">
+  
+    <form method="POST" action="misc/action_logout.php">
+    <button id="logout" type="submit" name="logout">LOGOUT</button>
+    </form>
+    <form method="POST" action="stats.php">
+    <button id="stats" type="submit" name="logout">STATS</button> 
+    </form>
+    <form method="POST" action="dash.php">
+    <button id="dash" type="submit" name="dash">Dash</button>
+    </form>
+    </div>
+  <div id="title"> 
+  <img src="../website/images/040.png"><br><h2>Resolution Error</h2>
+  <p>The device which you are using does not meet the required resolution for displaying this Webpage.</p>
+  </div>
+  
+</body>
+</html>
